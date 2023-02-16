f68a98a34447d92947ffc2f6d1f17e1ff598ea8c
mateus040 <mateusgabrielmoreno264@gmail.com>
2023-02-16 16:07:20 -0300

    teste JSON e criando funções

---
+++ Controller/EnderecoController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace ApiCep\Controller;
+
+use App\Model\{ EnderecoModel, CidadeModel };
+use Exception;
+
+class EnderecoController extends Controller
+{
+    public static function getByCepByLogradouro() : void /* void -> sem retorno (vazio) */
+    {
+
+    }
+
+    public static function getLogradouroByBairroAndCidade() : void
+    {
+
+    }
+
+    public static function getLogradouroByCep() : void
+    {
+
+    }
+
+    public static function getBairrosByIdCidade() : void
+    {
+
+    }
+
+    public static function getCidadesByUf() : void
+    {
+        
+    }
+}
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+include 'config.php';
+include 'autoload.php';
+include 'rotas.php';
+
+$cidades = ['Jaú', 'Bariri', 'Itapuí', 'Dois Córregos'];
+
+// var_dump($cidades);
+
+include 'Controller/Controller.php';
+
+\ApiCep\Controller\Controller::getResponseAsJSON($cidades);
